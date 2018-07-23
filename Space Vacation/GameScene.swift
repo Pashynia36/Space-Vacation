edee4270c2448abc789277d4fad53582f9808852
Pavlo Novak <pashanovak36@gmail.com>
2018-07-23 09:32:44 +0300

    Created project.

---
+++ Space Vacation/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Space Vacation
+//
+//  Created by Pavlo Novak on 7/23/18.
+//  Copyright © 2018 Pavlo Novak. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+
+	override func didMove(to view: SKView) {
+		let shuttle = SKSpriteNode(imageNamed: "shuttle")
+	}
+}
